0de2900ca7ae689cbf831343ce470621d0193442
Deepak Gouda <deepakkumargouda56@gmail.com>
2026-09-05 13:43:25 +0530

    Fix socket production URL

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://virtual-study-room-backend-3.onrender.com/api",{
+const socket = io("https://virtual-study-room-backend-3.onrender.com",{
   autoConnect: false,
 });
 
